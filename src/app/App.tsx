8cadbf5800ef0d4bc1f7caf9c33e5868228e33b7
Scott Mikula <mikula@gmail.com>
2022-09-19 22:10:27 -0700

    Add onFileChanged callback to StatsPicker

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,9 +3,13 @@ import { StatsPicker } from '../components/StatsPicker';
 import './App.css';
 
 export const App: React.FC<{}> = () => {
+    const onFileChanged = (data: any) => {
+        console.log(data);
+    };
+
     return (
         <div>
-            <StatsPicker />
+            <StatsPicker onFileChanged={onFileChanged} />
         </div>
     );
 };
--- src/components/StatsPicker.tsx
+++ src/components/StatsPicker.tsx
@@ -1,15 +1,17 @@
 import React, { ChangeEvent } from 'react';
 import { readJsonFile } from '../utils/readJsonFile';
 
-export const StatsPicker: React.FC<{}> = () => {
+export interface StatsPickerProps {
+    onFileChanged: (data: any) => void;
+}
+
+export const StatsPicker: React.FC<StatsPickerProps> = props => {
     const onFileChange = (event: ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files;
         if (!files || !files[0]) {
-            return;
+            props.onFileChanged(null);
         } else {
-            readJsonFile(files[0]).then(json => {
-                console.log(json);
-            });
+            readJsonFile(files[0]).then(props.onFileChanged);
         }
     };
 
