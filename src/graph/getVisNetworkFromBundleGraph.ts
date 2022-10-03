0980976c856b51a0c7d94d48b5646aee2ed1486f
Scott Mikula <mikula@gmail.com>
2022-10-03 09:33:42 -0700

    Optionally filter the rendered graph to a set of nodes

--- src/graph/getVisNetworkFromBundleGraph.ts
+++ src/graph/getVisNetworkFromBundleGraph.ts
@@ -1,14 +1,21 @@
 import { Data, Edge, Node } from 'vis-network';
 import { BundleGraph } from '../types/BundleGraph';
 
-export function getVizNetworkFromBundleGraph(bundleGraph: BundleGraph): Data {
+export function getVizNetworkFromBundleGraph(
+    bundleGraph: BundleGraph,
+    nodesToInclude?: Set<string>
+): Data {
     const nodes: Node[] = [];
     const edges: Edge[] = [];
 
     for (const node of bundleGraph.nodes.values()) {
-        nodes.push({ id: node.id, label: node.name || node.id });
-        for (const childId of node.children) {
-            edges.push({ from: node.id, to: childId });
+        if (!nodesToInclude || nodesToInclude.has(node.id)) {
+            nodes.push({ id: node.id, label: node.name || node.id });
+            for (const childId of node.children) {
+                if (!nodesToInclude || nodesToInclude.has(childId)) {
+                    edges.push({ from: node.id, to: childId });
+                }
+            }
         }
     }
 
