7645ae2a72664fef7abde52531ec1cef5c8e34e2
Scott Mikula <mikula@gmail.com>
2023-04-17 15:55:47 -0700

    Fill in all edges to/from the added node

--- package/src/bundle-graph-explorer/addChunkGroupToGraph.ts
+++ package/src/bundle-graph-explorer/addChunkGroupToGraph.ts
@@ -5,23 +5,40 @@ import { GraphData } from './getGraphData';
 export function addChunkGroupToGraph(graphData: GraphData, chunkGroupId: string, parentId: string) {
     const nodes = graphData.visData.nodes as DataSet<Node>;
     const edges = graphData.visData.edges as DataSet<Edge>;
+    const chunkGroup = graphData.chunkGroupMap.get(chunkGroupId)!;
 
     // Add the node
     if (!nodes.get(chunkGroupId)) {
-        const chunkGroup = graphData.chunkGroupMap.get(chunkGroupId);
         nodes.add({
             id: chunkGroupId,
             label: chunkGroup!.name || chunkGroupId,
         });
     }
 
-    // Add the edge
-    const edgeId = `${parentId}->${chunkGroupId}`;
+    // Fill in edges to/from this node
+    nodes.forEach(node => {
+        const otherChunkGroupId = node.id! as string;
+        const otherChunkGroup = graphData.chunkGroupMap.get(otherChunkGroupId)!;
+
+        // Check for edge from parent
+        if (otherChunkGroup.children.includes(chunkGroupId)) {
+            addEdge(edges, otherChunkGroupId, chunkGroupId);
+        }
+
+        // Check for edge to child
+        if (chunkGroup.children.includes(otherChunkGroupId)) {
+            addEdge(edges, chunkGroupId, otherChunkGroupId);
+        }
+    });
+}
+
+function addEdge(edges: DataSet<Edge>, parentId: string, childId: string) {
+    const edgeId = `${parentId}->${childId}`;
     if (!edges.get(edgeId)) {
         edges.add({
             id: edgeId,
             from: parentId,
-            to: chunkGroupId,
+            to: childId,
         });
     }
 }
