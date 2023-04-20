cb51d595f2ebac15517c73e23d91b0223568c9e0
Scott Mikula <mikula@gmail.com>
2023-04-19 17:50:49 -0700

    Do a better job of getting entry bundles

--- package/src/bundle-graph-explorer/getEntryBundles.ts
+++ package/src/bundle-graph-explorer/getEntryBundles.ts
@@ -1,14 +1,7 @@
 import type { ChunkGroupMap } from './getChunkGroupMap';
 
 export function getEntryBundles(chunkGroupMap: ChunkGroupMap) {
-    const possibleEntryBundles = new Set([...chunkGroupMap.values()].filter(cg => cg.name));
-
-    // Filter out those that are the child of another
-    for (let cg of chunkGroupMap.values()) {
-        for (let childId of cg.children) {
-            possibleEntryBundles.delete(chunkGroupMap.get(childId)!);
-        }
-    }
-
-    return possibleEntryBundles;
+    return [...chunkGroupMap.values()].filter(
+        cg => cg.chunkGroupType === 'Entrypoint' && cg.name && !cg.name.includes('node_modules')
+    );
 }
