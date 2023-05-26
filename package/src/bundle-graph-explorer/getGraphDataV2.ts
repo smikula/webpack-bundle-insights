782f40b5f6bf898ff9f4128d52c40ab62a599444
Scott Mikula <mikula@gmail.com>
2023-05-25 19:49:07 -0700

    Finish implementing getGraphDataV2

--- package/src/bundle-graph-explorer/getGraphDataV2.ts
+++ package/src/bundle-graph-explorer/getGraphDataV2.ts
@@ -1,6 +1,7 @@
 import { Edge, Node } from 'vis-network';
 import { EnhancedBundleStats } from '../enhanced-bundle-stats/EnhancedBundleStats';
 
+// TODO: Rename
 export function getGraphDataV2(stats: EnhancedBundleStats, chunkGroupsToShow: string[]) {
     const nodes: Node[] = [];
     const edges: Edge[] = [];
@@ -11,7 +12,22 @@ export function getGraphDataV2(stats: EnhancedBundleStats, chunkGroupsToShow: st
     }
 
     for (let chunkGroupId of chunkGroupsToShow) {
-        // TODO: Create nodes, follow edges to other shown bundles
+        const chunkGroup = stats.getChunkGroup(chunkGroupId)!;
+        nodes.push({
+            id: chunkGroupId,
+            label: chunkGroup.name || chunkGroup.id,
+        });
+
+        for (let childId of chunkGroup.children) {
+            if (chunkGroupsToShow.includes(childId)) {
+                const edgeId = `${chunkGroupId}->${childId}`;
+                edges.push({
+                    id: edgeId,
+                    from: chunkGroupId,
+                    to: childId,
+                });
+            }
+        }
     }
 
     return { nodes, edges };
