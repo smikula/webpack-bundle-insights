03b65a14a0d2336fd84c4c2fbe8a6e9146cf4a86
Scott Mikula <mikula@gmail.com>
2024-03-13 13:30:12 -0700

    Factor out BundleGraphExplorerPane

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -1,26 +1,6 @@
-import React from 'react';
-import { BundleStats } from 'webpack-bundle-stats-plugin';
-import { BundleGraphExplorer } from 'webpack-bundle-insights';
-import { StatsPicker } from '../components/StatsPicker';
+import { BundleGraphExplorerPane } from './BundleGraphExplorerPane';
 import './App.css';
 
 export const App: React.FC<{}> = () => {
-    const [stats, setStats] = React.useState<BundleStats>();
-
-    const onFileChanged = (stats: BundleStats) => {
-        if ((stats as any).bundleDataV2) {
-            stats = (stats as any).bundleDataV2;
-        }
-
-        setStats(stats);
-    };
-
-    return (
-        <div className="container">
-            <div className="sidebar">
-                <StatsPicker onFileChanged={onFileChanged} />
-            </div>
-            <BundleGraphExplorer stats={stats} className="graph" />
-        </div>
-    );
+    return <BundleGraphExplorerPane />;
 };
