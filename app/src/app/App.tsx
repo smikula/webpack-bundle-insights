5e694b7262ea97648738e7f2544230e4db939afe
Scott Mikula <mikula@gmail.com>
2024-03-13 14:04:18 -0700

    Add a mode selector

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -1,13 +1,20 @@
+import React from 'react';
 import { BundleGraphExplorerPane } from './BundleGraphExplorerPane';
 import './App.css';
 
 export const App: React.FC<{}> = () => {
+    const [mode, setMode] = React.useState('none');
+
+    const onSelectMode = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setMode(event.target.value);
+    };
+
     return (
         <>
-            <BundleGraphExplorerPane />
-            <select className="selector">
-                <option>Hi</option>
-                <option>Bye</option>
+            {mode == 'explore' ? <BundleGraphExplorerPane /> : null}
+            <select className="selector" onChange={onSelectMode}>
+                <option value="none">Select mode</option>
+                <option value="explore">Bundle graph explorer</option>
             </select>
         </>
     );
