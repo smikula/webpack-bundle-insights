8df2f9dab2c9137a612548c5cb8a48d833892bbe
Scott Mikula <mikula@gmail.com>
2024-03-13 13:50:01 -0700

    Add selector

--- app/src/app/App.tsx
+++ app/src/app/App.tsx
@@ -2,5 +2,13 @@ import { BundleGraphExplorerPane } from './BundleGraphExplorerPane';
 import './App.css';
 
 export const App: React.FC<{}> = () => {
-    return <BundleGraphExplorerPane />;
+    return (
+        <>
+            <BundleGraphExplorerPane />
+            <select className="selector">
+                <option>Hi</option>
+                <option>Bye</option>
+            </select>
+        </>
+    );
 };
