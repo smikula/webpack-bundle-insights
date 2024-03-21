882a87f4c1c4dc4c96a407ef47afa8a171f6db9b
Scott Mikula <mikula@gmail.com>
2024-03-21 15:58:08 -0700

    Update supported range for webpack-bundle-stats-plugin (#22)

--- package/src/bundle-graph-diff/components/BundleGraphDiff.tsx
+++ package/src/bundle-graph-diff/components/BundleGraphDiff.tsx
@@ -13,7 +13,7 @@ export interface BundleGraphDiffProps {
     className?: string;
 }
 
-const SUPPORTED_RANGE = '~0.3.0';
+const SUPPORTED_RANGE = '>0.3.0';
 
 const graphOptions: Options = {
     layout: {
